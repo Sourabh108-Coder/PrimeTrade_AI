a1e016622815a6d8218c2759056f365547861319
Sourabh108-Coder <kumarsourabh7164@gmail.com>
2026-01-17 14:33:19 +0530

    Update User Model

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,16 +4,19 @@ const userSchema = new mongoose.Schema(
 {
   name: { 
     type: String, 
-    required: true },
+    required: true 
+  },
 
   email: { 
     type: String, 
     required: true, 
-    unique: true },
+    unique: true 
+  },
     
   password: { 
     type: String, 
-    required: true },
+    required: true 
+  },
 
 });
 
